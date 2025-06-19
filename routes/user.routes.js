0ade1cee5d0b2026a580218249b83e23d37a9762
yakir3160 <132565923+yakir3160@users.noreply.github.com>
2025-06-19 15:14:20 +0300

    feat: Import upload middleware for handling profile image uploads

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getAllUsers, userActionById } from '../controller/user.controller.js'
+import { uploadSingleFile, handleProfileImgUpload } from "../middleware/upload.middleware.js";
 
 const router = Router();
 
