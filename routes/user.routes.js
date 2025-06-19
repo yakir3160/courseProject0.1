fe9e758ace76af5bc276c2194e27dad0bcee259d
yakir3160 <132565923+yakir3160@users.noreply.github.com>
2025-06-19 14:07:08 +0300

    refactor: Improve code formatting and structure in user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,25 @@
 import { Router } from "express";
-import {getAllUsers,userActionById} from '../controller/user.controller.js'
+import { getAllUsers, userActionById } from '../controller/user.controller.js'
+
 const router = Router();
 
-router.get('/',getAllUsers)
+router.get('/', getAllUsers)
 
 
 
-router.get('/:id',userActionById);
+router.get('/:id', userActionById);
 router.put('/:id', userActionById);
 router.delete('/:id', userActionById);
-router.put('/upload', userActionById);
 
+
+router.post('/upload/:id',
+    uploadSingleFile,
+    handleProfileImgUpload,
+    (req, res, next) => {
+        req.method = 'PUT'
+        userActionById(req, res, next);
+    }
+
+);
 
 export default router
